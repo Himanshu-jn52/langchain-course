04f30468dcf97846886c8c54906d07579a278379
Himanshu-jn52 <himanjn52@gmail.com>
2025-10-18 19:34:52 +0530

    adding structured output

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
-from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
@@ -16,19 +15,18 @@ dotenv.load_dotenv()
 tools = [TavilySearch()]
 
 llm = ChatOpenAI(model="gpt-4o-mini")
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
 
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tools", "tool_names"],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 agent = create_react_agent(llm, tools, prompt=react_prompt_with_format_instructions)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 extract_output = RunnableLambda(lambda x : x["output"])
-parse_output = RunnableLambda(lambda x : output_parser.parse(x))
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
